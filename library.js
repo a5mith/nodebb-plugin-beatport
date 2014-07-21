68ecb6a9ba5be70e811aca82d8dd47082e45a937
a5mith <aaronsmith91@sky.com>
2014-07-21 14:58:07 +0100

    Wrong expression.

--- library.js
+++ library.js
@@ -1,12 +1,12 @@
     (function(module) {
         "use strict";
         var Beatport = {},
-            embedtrack = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$1&type=track&auto=0"></iframe>',
-            embedmix = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$1&type=mix&auto=0"></iframe>';
+            embedtrack = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=track&auto=0"></iframe>',
+            embedmix = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=mix&auto=0"></iframe>';
 
         Beatport.parse = function(postContent, callback) {
-            var	track = /<a href="(?:http?:\/\/)?(?:www\.)?(?:beatport\.com)\/track\/([\w\-_]+)">.+<\/a>/g;
-            var	mix = /<a href="(?:http?:\/\/)?(?:www\.)?(?:beatport\.com)\/mix\/([\w\-_]+)">.+<\/a>/g;
+            var	track = /<a href="(?:http?:\/\/)?(?:www\.)?(?:beatport\.com)\/track\/([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
+            var	mix = /<a href="(?:http?:\/\/)?(?:www\.)?(?:beatport\.com)\/mix\/([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
 
             if (postContent.match(track)) {
                 postContent = postContent.replace(track, embedtrack);
