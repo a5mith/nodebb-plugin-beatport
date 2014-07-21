9dbf89bae12e699d7d819db97e996c1063639ae9
a5mith <aaronsmith91@sky.com>
2014-07-21 15:09:40 +0100

    missed the var

--- library.js
+++ library.js
@@ -1,8 +1,8 @@
     (function(module) {
         "use strict";
         var Beatport = {},
-            embedtrack = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=track&auto=0"></iframe>',
-            embedmix = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=mix&auto=0"></iframe>';
+            embedtrack = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=track&auto=0" width="100%" height="162" frameborder="0" scrolling="no"></iframe>',
+            embedmix = '<iframe class="beatport" src="http://embed.beatport.com/player?id=$2&type=mix&auto=0" width="100%" height="162" frameborder="0" scrolling="no"></iframe>';
 
         Beatport.parse = function(postContent, callback) {
             var	track = /<a href="(?:http?:\/\/)?(?:www\.)?(?:beatport\.com)\/track\/([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
@@ -12,7 +12,7 @@
                 postContent = postContent.replace(track, embedtrack);
             }
             if (postContent.match(mix)) {
-                postContent = postContent.replace(mix, embed);
+                postContent = postContent.replace(mix, embedmix);
             }
 
             callback(null, postContent);
@@ -23,3 +23,4 @@
 
 
 
+    <iframe src="http://embed.beatport.com/player/?id=178484&type=mix" width="100%" height="162" frameborder="0" scrolling="no" style="max-width:600px;"></iframe>
